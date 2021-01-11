7f4cc74c1035c925a6402ae7b6fcb3449511f84c
Menachem Yudborovski <Menachem.Yudborovski@amdocs.com>
2021-01-11 16:01:49 +0200

    add components and screens

---
+++ pick-a-job-frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = (props) => {
+    
+
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        Copyright &copy; Pick-A-Job
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer;

2aa57024ab3e94a8b4cb694352e3ccb1e2c07b1e
Meni <meni@pcysys.com>
2021-10-15 12:28:24 +0300

    production preperations

--- pick-a-job-backend/server.js
+++ pick-a-job-backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
@@ -15,13 +16,23 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-});
-
 app.use('/api/positions', positionsRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/companies', companyRoutes);
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/pick-a-job-frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'pick-a-job-frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)

07977a700f0f05a9ed8828dcd9f168811e9f3f42
Meni <meni@pcysys.com>
2021-10-23 13:55:44 +0300

    restrict list companies by related company

--- pick-a-job-backend/utils/consts.js
+++ pick-a-job-backend/utils/consts.js
@@ -41,6 +41,11 @@ const isUserPowerAdminRole = (role) => {
     return role === Roles.PAJADMIN;
 }
 
+// isUserCompanyAdminRole receives string and returns true iff the string is one of [COMPANYADMIN]
+const isUserCompanyAdminRole = (role) => {
+    return role === Roles.COMPANYADMIN;
+}
+
 const getTokenFromRequest = (req) => {
     let token = null;
     const authHeader = req?.headers?.authorization;
@@ -86,5 +91,6 @@ export {Roles,
     aggregatePositionStatus,
     getTokenFromRequest,
     findCreateePropsFromCreator,
-    isUserPowerAdminRole
+    isUserPowerAdminRole,
+    isUserCompanyAdminRole
 }
